97fbdc8fee9da9151a35fb4aab40fcb4d5938695
SnirGa <snirga@post.bgu.ac.il>
2023-04-21 16:53:55 +0300

    changed username and password

--- Service/LoginPageService.py
+++ Service/LoginPageService.py
@@ -1,18 +1,23 @@
 from selenium.webdriver.common.by import By
 
+
 class LoginPageService:
-    def __init__(self,chrome_service):
-        self.chrome_service=chrome_service
+    def __init__(self, chrome_service):
+        self.chrome_service = chrome_service
     """Login the default user:
     """
+
     def login_user(self):
         self.chrome_service.move_to_url("https://irony.cs.bgu.ac.il/#/login")
         # sets username field
-        username = self.chrome_service.driver.find_element(By.XPATH, "/html/body/div/div/div/form/div[1]/div/input")
-        username.send_keys("chana")
+        username = self.chrome_service.driver.find_element(
+            By.XPATH, "/html/body/div/div/div/form/div[1]/div/input")
+        username.send_keys("xxx")
         # sets password field
-        password = self.chrome_service.driver.find_element(By.XPATH, "/html/body/div[1]/div/div/form/div[2]/div/input")
-        password.send_keys("banana")
+        password = self.chrome_service.driver.find_element(
+            By.XPATH, "/html/body/div[1]/div/div/form/div[2]/div/input")
+        password.send_keys("xxx")
         # clicks on submit button
-        login_button = self.chrome_service.driver.find_element(By.XPATH, "/html/body/div[1]/div/div/form/button")
+        login_button = self.chrome_service.driver.find_element(
+            By.XPATH, "/html/body/div[1]/div/div/form/button")
         login_button.click()
